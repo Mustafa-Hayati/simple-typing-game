5bbfd7acb1f9b73175e06edb8718aa9f45ddd898
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-10-26 12:14:13 +0330

    Add the character by charcter comparison.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,28 @@ import { lorem } from "faker";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  sentence = lorem.sentence();
+  randomText = lorem.sentence();
+  letters = this.randomText.split("").map((letter) => {
+    return {
+      letter,
+      className: "",
+    };
+  });
+  enteredText = "";
+  success = false;
+
+  onTextInput(e: Event): void {
+    this.letters.forEach((letter) => (letter.className = ""));
+    this.enteredText = (e.target as HTMLInputElement).value;
+    this.success = this.enteredText === this.randomText;
+
+    for (let i = 0; i < this.enteredText.length; i++) {
+      if (this.randomText.includes(this.enteredText[i])) {
+        this.letters[i].className =
+          this.randomText[i] === this.enteredText[i] ? "correct" : "wrong";
+      } else {
+        this.letters[i].className = "wrong";
+      }
+    }
+  }
 }
